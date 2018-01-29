4cef44fb5c7e3ebf1ed9628dae8c5a05e427dcaf
J4m3sC95 <jcoulson95@gmail.com>
2018-01-29 21:11:02 +0000

    started to add stuff to gtk interface

--- CAN_LINUX_Interface/CAN_Interface.c
+++ CAN_LINUX_Interface/CAN_Interface.c
@@ -7,23 +7,19 @@
 #include <string.h>
 #include <unistd.h>
 
-/* baudrate settings are defined in <asm/termbits.h>, which is
-included by <termios.h> */
-#define BAUDRATE B9600  
-/* change this definition for the correct port */
-#define MODEMDEVICE "/dev/ttyACM0"
-#define _POSIX_SOURCE 1 /* POSIX compliant source */
+#include <gtk/gtk.h>
 
-#define FALSE 0
-#define TRUE 1
+#define BAUDRATE B9600  
+#define MODEMDEVICE "/dev/ttyACM0"
+#define _POSIX_SOURCE 1
 
 volatile int STOP=FALSE; 
 
-int main()
-{
-	int fd, res;
+int fd, res;
 	struct termios oldtio,newtio;
 	char buf[255];
+
+void serial_setup(){
 	//  Open modem device for reading and writing and not as controlling tty  because we don't want to get killed if linenoise sends CTRL-C.
 	 fd = open(MODEMDEVICE, O_RDWR | O_NOCTTY ); 
 	 if (fd <0) {perror(MODEMDEVICE); exit(-1); }
@@ -48,7 +44,71 @@ int main()
 	// now clean the modem line and activate the settings for the port
 	 tcflush(fd, TCIFLUSH);
 	 tcsetattr(fd,TCSANOW,&newtio);
-	 char data = 'a';
+ }
+
+GdkPixbuf *create_pixbuf(const gchar * filename) {
+    
+   GdkPixbuf *pixbuf;
+   GError *error = NULL;
+   pixbuf = gdk_pixbuf_new_from_file(filename, &error);
+   
+   if (!pixbuf) {
+       
+      fprintf(stderr, "%s\n", error->message);
+      g_error_free(error);
+   }
+
+   return pixbuf;
+}
+
+void send_bytes(GtkWidget *widget, gpointer window){
+	buf[0] = 'a';
+	buf[1] = 'b';
+	 buf[2] = '\n';
+	 write(fd, buf, 2);
+	 res = read(fd,buf,255); 
+	buf[res]=0;
+	printf("%s", buf);
+ }
+
+int main(int argc, char *argv[])
+{
+	serial_setup();
+	
+	GtkWidget *window;
+	GtkWidget *halign;
+	GtkWidget *button;
+	GdkPixbuf *icon;
+	
+	gtk_init(&argc, &argv);
+
+	window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+	gtk_window_set_title(GTK_WINDOW(window), "CAN Interface");
+	gtk_window_set_default_size(GTK_WINDOW(window), 300,200);
+	gtk_container_set_border_width(GTK_CONTAINER(window), 15);
+	gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
+	
+	icon = create_pixbuf("led_red.png");  
+	gtk_window_set_icon(GTK_WINDOW(window), icon);
+	
+	button = gtk_button_new_with_label("Button");
+	
+	halign = gtk_alignment_new(0, 0, 0, 0);
+	gtk_container_add(GTK_CONTAINER(halign), button);
+	gtk_container_add(GTK_CONTAINER(window), halign);  
+	
+	g_signal_connect(button, "clicked",  G_CALLBACK(send_bytes), NULL); 
+	
+	gtk_widget_show_all(window);
+  
+	g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);  
+	
+
+	gtk_main();
+	
+	g_object_unref(icon);    	
+	
+	 char data = 'b';
 
 	 while (STOP==FALSE) {		 
 		 buf[0] = data;
@@ -56,11 +116,11 @@ int main()
 		 write(fd, buf, 2);
 		 data++;
 		res = read(fd,buf,255); 
-		buf[res]=0;             /* set end of string, so we can printf */
+		buf[res]=0;
 		printf("%s", buf);
 		if (buf[0]=='z') STOP=TRUE;
 	 }
-	 /* restore the old port settings */
+	 // restore the old port settings
 	 tcsetattr(fd,TCSANOW,&oldtio);
 	 
 	 return 0;
